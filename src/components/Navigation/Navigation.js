078087169365d72f41a07dc580a33a4cc2688afd
Antonio Valverde <antonio@valverdebalbuena.es>
2023-07-25 11:37:45 +0200

    Update Navigation.js fix errata

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
 		return (
 			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 				<p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'> Conectar </p>
-				<p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'> Register </p>
+				<p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'> Registrar </p>
 			</nav>	
 		);			
 	}
